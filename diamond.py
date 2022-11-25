a793fd166d5f38026bc9aab1ac4808d92b53810a
Eugene Crabs <eugene_crabs@Eugenes-MacBook-Air.local>
2022-11-25 13:40:34 +0300

    Added a welcome function to welcome the user

--- diamond.py
+++ diamond.py
@@ -1,7 +1,11 @@
+from welcome import welcome
+
 class Diamond:
     
     def __init__(self):
         self.top_shapes = []
+        self.welcome = welcome()
+        
     
     def engage_user(self):
         number = int(input("Enter an odd number: "))
@@ -11,26 +15,15 @@ class Diamond:
         else:
             self.diamond(number)
         
-    def try_again(self):
-        ask = input("Do you want to give it another shot?[y/n] ").lower()
-        if ask == 'y':
-            self.engage_user()
-        elif ask == 'n':
-            print("Bye")
-        else:
-            self.try_again()
-        
     def diamond(self,number):
         loop_times = self.get_loop_time(number)
         spaces_to_add = loop_times
         diamond_to_add = 1
 
         for time in range(loop_times +1):
-            
             to_append = self.generate_position_shape(spaces_to_add,diamond_to_add)
             spaces_to_add-=1
             diamond_to_add+=2
-            
             self.loop_time_operations(time,loop_times,to_append)
         self.try_again()
             
@@ -59,7 +52,14 @@ class Diamond:
     def is_odd(self,number):
         return number % 2 != 0        
 
-
+    def try_again(self):
+        ask = input("Do you want to give it another shot?[y/n] ").lower()
+        if ask == 'y':
+            self.engage_user()
+        elif ask == 'n':
+            print("Bye")
+        else:
+            self.try_again()
     
 dmd = Diamond()
 dmd.engage_user()
