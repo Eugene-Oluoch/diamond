6a64a1c6d0932c963bdd988bfa909eea22da2e5d
Eugene Crabs <eugene_crabs@Eugenes-MacBook-Air.local>
2022-11-25 13:32:21 +0300

    Added a try again method

--- diamond.py
+++ diamond.py
@@ -6,22 +6,33 @@ class Diamond:
     def engage_user(self):
         number = int(input("Enter an odd number: "))
         if not self.is_odd(number):
+            print("Give a valid odd number")
             self.engage_user()
         else:
             self.diamond(number)
+        
+    def try_again(self):
+        ask = input("Do you want to give it another shot?[y/n] ").lower()
+        if ask == 'y':
+            self.engage_user()
+        elif ask == 'n':
+            print("Bye")
+        else:
+            self.try_again()
         
     def diamond(self,number):
         loop_times = self.get_loop_time(number)
         spaces_to_add = loop_times
         diamond_to_add = 1
 
-        for _ in range(loop_times +1):
+        for time in range(loop_times +1):
             
             to_append = self.generate_position_shape(spaces_to_add,diamond_to_add)
             spaces_to_add-=1
             diamond_to_add+=2
             
-            self.loop_time_operations(_,loop_times,to_append)
+            self.loop_time_operations(time,loop_times,to_append)
+        self.try_again()
             
 
     def loop_time_operations(self,time, loop_times, to_append):
@@ -40,6 +51,7 @@ class Diamond:
         reversed_array = self.top_shapes[::-1]
         for num in reversed_array:
             print(num)
+        self.top_shapes = []
             
     def get_loop_time(self,number):
         return number // 2
