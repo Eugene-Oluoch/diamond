2afbb7fd0ea7c775badd8d2609e8b023aeb15fed
Eugene Crabs <eugene_crabs@Eugenes-MacBook-Air.local>
2022-11-25 12:50:39 +0300

    Initial commit

---
+++ diamond.py
@@ -0,0 +1,41 @@
+class Diamond:
+    def engage_user(self):
+        number = int(input("Enter an odd number: "))
+        self.diamond(number)
+        
+    def diamond(self,number):
+        if self.is_odd(number):
+            loop_times = number // 2
+            spaces_to_add = loop_times
+            diamond_to_add = 1
+            array = []
+            for _ in range(loop_times +1):
+                to_append = self.generate_position_shape(spaces_to_add,diamond_to_add)
+                spaces_to_add-=1
+                diamond_to_add+=2
+                if _ < loop_times:
+                    array.append(to_append)
+                print(to_append)
+                if _ == loop_times:
+                    self.print_last_portion(array)
+        else:
+            print("Give a valid odd number")
+            self.engage_user()
+
+    def generate_position_shape(self,spaces_to_add,diamond_to_add):
+        spaces = " " * spaces_to_add
+        diamond = "*" * diamond_to_add
+        return spaces + diamond
+
+    def print_last_portion(self,array):
+        reversed_array = array[::-1]
+        for num in reversed_array:
+            print(num)
+            
+    def is_odd(self,number):
+        return number % 2 != 0        
+
+
+    
+dmd = Diamond()
+dmd.engage_user()
