22dea9a6d424f2b2e0ca4f9f18cd121177f4a782
Eugene Crabs <eugene_crabs@Eugenes-MacBook-Air.local>
2022-11-25 12:59:56 +0300

    Refactored the code

--- diamond.py
+++ diamond.py
@@ -1,34 +1,35 @@
 class Diamond:
     def engage_user(self):
         number = int(input("Enter an odd number: "))
-        self.diamond(number)
+        if not self.is_odd(number):
+            self.engage_user()
+        else:
+            self.diamond(number)
         
     def diamond(self,number):
-        if self.is_odd(number):
-            loop_times = self.get_loop_time(number)
-            spaces_to_add = loop_times
-            diamond_to_add = 1
-            array = []
-            for _ in range(loop_times +1):
-                to_append = self.generate_position_shape(spaces_to_add,diamond_to_add)
-                spaces_to_add-=1
-                diamond_to_add+=2
-                if _ < loop_times:
-                    array.append(to_append)
-                print(to_append)
-                if _ == loop_times:
-                    self.print_last_portion(array)
-        else:
-            print("Give a valid odd number")
-            self.engage_user()
+        top_shapes = []
+        loop_times = self.get_loop_time(number)
+        spaces_to_add = loop_times
+        diamond_to_add = 1
+
+        for _ in range(loop_times +1):
+            to_append = self.generate_position_shape(spaces_to_add,diamond_to_add)
+            spaces_to_add-=1
+            diamond_to_add+=2
+            if _ < loop_times:
+                top_shapes.append(to_append)
+            print(to_append)
+            if _ == loop_times:
+                self.print_last_portion(top_shapes)
+            
 
     def generate_position_shape(self,spaces_to_add,diamond_to_add):
         spaces = " " * spaces_to_add
         diamond = "*" * diamond_to_add
         return spaces + diamond
 
-    def print_last_portion(self,array):
-        reversed_array = array[::-1]
+    def print_last_portion(self,top_shapes):
+        reversed_array = top_shapes[::-1]
         for num in reversed_array:
             print(num)
             
